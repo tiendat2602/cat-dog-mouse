4fa2a8d9c89904fb0b9150893fad8d61c1c587f4
tiendat2602 <nguyentiendatbk2004@gmail.com>
2019-11-17 23:36:08 +0700

    improve eat method on Cat class

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const Dog = require('./Dog');
 const Cat = require('./Cat');
+const Mouse = require('./Mouse');
 
 const dog = new Dog("Lucy");
 const cat = new Cat("Tom");
+const mouse = new Mouse("Mickey");
+
+try{
+	cat.eat(dog);
+} catch(error) {
+	console.log(error.message);
+}
+
+console.log(cat);
+
+
 
 
 
